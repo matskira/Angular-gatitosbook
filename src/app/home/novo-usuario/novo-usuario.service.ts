c8ef77c6a542133f6b5bcc0fa2796cb17853ce22
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-14 12:23:02 -0300

    Adicionado formulário reativo para o cadastro de usuário

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,19 @@
+import { NovoUsuario } from './novo-usuario';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private httpClient: HttpClient) { }
+
+
+  cadastrarNovoUsuario(novoUsuario: NovoUsuario): Observable<any>{
+    return this.httpClient.post('http://localhost:3000/user/signup', novoUsuario);
+  }
+
+
+}
