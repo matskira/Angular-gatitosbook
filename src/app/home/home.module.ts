05199aad4b22839154fe15fe93fa862f93bbf0ec
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-14 10:32:38 -0300

    Adicionado validação dos campos usuário e senha, através de um Template Driven

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,3 +1,4 @@
+import { MensagemModule } from './../componentes/mensagem/mensagem.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -12,7 +13,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     HomeRoutingModule,
-    FormsModule
+    FormsModule,
+    MensagemModule
   ],
 })
 export class HomeModule { }
