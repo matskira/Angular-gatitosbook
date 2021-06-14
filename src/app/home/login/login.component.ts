9ae260d9c93a75d4621ae7c1601df0f621dda927
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-14 09:16:40 -0300

    Adicionado autenticação por API, também adicionamos o zip da api node que utilizamos 'api-usuario'

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,27 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  usuario = '';
+  senha = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      ()=>{
+        console.log('Autenticação realizada com sucesso');
+      }, (error) => {
+        alert('Usuário ou senha inválidos');
+        console.log(error);
+      }
+    );
   }
-
 }
