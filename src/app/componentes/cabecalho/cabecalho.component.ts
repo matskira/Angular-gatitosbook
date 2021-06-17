e80dd3f7fb968000ce79529244b5a728a446ad73
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-17 09:01:40 -0300

    Adicionado cabeçalho e rodapé, além de informações do usuário quando logado

--- src/app/componentes/cabecalho/cabecalho.component.ts
+++ src/app/componentes/cabecalho/cabecalho.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from './../../autenticacao/usuario/usuario.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cabecalho',
   templateUrl: './cabecalho.component.html',
-  styleUrls: ['./cabecalho.component.css']
+  styleUrls: ['./cabecalho.component.css'],
 })
-export class CabecalhoComponent implements OnInit {
+export class CabecalhoComponent {
+  user$ = this.usuarioService.retornaUsuario();
 
-  constructor() { }
+  constructor(private usuarioService: UsuarioService, private router: Router) {}
 
-  ngOnInit(): void {
+  logout() {
+    this.usuarioService.logout();
+    this.router.navigate(['']);
   }
-
 }
