a4e3817fa0918cef42aca28c1b9e2a13aa254f8f
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-18 09:41:21 -0300

    Atualizado nosso arquivo enviromnent com o endereço local da nossa API e utilização de boas práticas

---
+++ src/app/animais/animal.ts
@@ -0,0 +1,12 @@
+export interface Animal {
+  id: number;
+  postDate: Date;
+  url: string;
+  description: string;
+  allowComments: string;
+  likes: number;
+  comments: number;
+  userId: number;
+}
+
+export type Animais = Array<Animal>;
--- src/app/home/novo-usuario/novo-usuario.service.ts
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -1,7 +1,10 @@
+import { environment } from './../../../environments/environment';
 import { NovoUsuario } from './novo-usuario';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+
+const API = environment.apiURL;
 
 @Injectable({
   providedIn: 'root',
@@ -10,13 +13,12 @@ export class NovoUsuarioService {
   constructor(private httpClient: HttpClient) {}
 
   cadastrarNovoUsuario(novoUsuario: NovoUsuario): Observable<any> {
-    return this.httpClient.post(
-      'http://localhost:3000/user/signup',
-      novoUsuario
-    );
+    return this.httpClient.post(`${API}/user/signup`, novoUsuario);
   }
 
   verificaUsuarioExistente(nomeUsuario: string) {
-    return this.httpClient.get(`http://localhost:3000/user/exists/${nomeUsuario}`);
+    return this.httpClient.get(
+      `${API}/user/exists/${nomeUsuario}`
+    );
   }
 }
