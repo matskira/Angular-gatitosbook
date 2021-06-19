49b5950f40c71444d0ecaadb8ba835c8759a8bbe
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-19 10:48:36 -0300

    Componente do deatlhe animal e template criados

--- src/app/animais/animais-routing.module.ts
+++ src/app/animais/animais-routing.module.ts
@@ -1,3 +1,4 @@
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
@@ -7,6 +8,10 @@ const routes: Routes = [
     path: '',
     component: ListaAnimaisComponent,
   },
+  {
+    path: 'animaisID',
+    component: DetalheAnimalComponent
+  }
 ];
 
 @NgModule({
