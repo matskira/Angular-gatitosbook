7f548b6d045a92341ec24fd31e2067ce28f2a7c4
Matheus Mariano Poda <matheus.poda@gmail.com>
2021-06-19 14:24:50 -0300

    Registrando o interceptor em nosso module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 import { RodapeModule } from './componentes/rodape/rodape.module';
 import { CabecalhoModule } from './componentes/cabecalho/cabecalho.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -15,6 +16,7 @@ import { HttpClientModule } from '@angular/common/http';
     HttpClientModule,
     CabecalhoModule,
     RodapeModule,
+    AutenticacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/autenticacao/autenticacao.interceptor.ts
+++ src/app/autenticacao/autenticacao.interceptor.ts
@@ -5,21 +5,23 @@ import {
   HttpHandler,
   HttpEvent,
   HttpInterceptor,
-  HttpHeaders
+  HttpHeaders,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AutenticacaoInterceptor implements HttpInterceptor {
-
   constructor(private tokenService: TokenService) {}
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
     if (this.tokenService.possuiToken()) {
       const token = this.tokenService.retornaToken();
       const headers = new HttpHeaders().append('x-acess-token', token);
-
+      request = request.clone({ headers });
+    }
     return next.handle(request);
   }
 }
